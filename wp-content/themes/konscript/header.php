b357f647ac97d4f77023188afac52225d3015011
Thomas Didriksen <t.didriksen@gmail.com>
2012-03-18 23:32:56 +0100

    Changed doctype on header. Hopefully this will please IE.

--- wp-content/themes/konscript/header.php
+++ wp-content/themes/konscript/header.php
@@ -5,7 +5,7 @@
  * Master template for head-section and body opening with generic elements
  */
 ?>
-<!-- <!DOCTYPE html> -->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 	<head>	
 		<base href="<?php echo get_bloginfo( 'wpurl' ); ?>/" />
