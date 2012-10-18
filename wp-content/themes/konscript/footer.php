63f016483742b37cca548bba8ebd9891d1fe4a53
laander <la@laander.com>
2012-10-18 23:56:47 +0200

    Corrected e-mail to .net in footer

--- wp-content/themes/konscript/footer.php
+++ wp-content/themes/konscript/footer.php
@@ -26,7 +26,7 @@
 					<iframe id="ff7511aa" name="f34edd7794" scrolling="no" style="border-width: initial; border-color: initial; overflow-x: hidden; overflow-y: hidden; border-width: initial; border-color: initial; border-width: initial; border-color: initial; border-width: initial; border-color: initial; height: 21px; width: 77px; border-top-style: none; border-right-style: none; border-bottom-style: none; border-left-style: none; border-width: initial; border-color: initial; " title="Like this content on Facebook." class="fb_ltr" src="http://www.facebook.com/plugins/like.php?api_key=113869198637480&amp;channel_url=http%3A%2F%2Fstatic.ak.fbcdn.net%2Fconnect%2Fxd_proxy.php%23cb%3Df18b68036c%26origin%3Dhttp%253A%252F%252Fdevelopers.facebook.com%252Ff3903f010%26relation%3Dparent.parent%26transport%3Dpostmessage&amp;colorscheme=dark&amp;font=arial&amp;href=http%3A%2F%2Ffacebook.com%2Faquestionof&amp;layout=button_count&amp;locale=en_US&amp;node_type=link&amp;sdk=joey&amp;show_faces=false&amp;width=77"></iframe>
 				</div>								
 				
-				<div class="meta">A QUESTION OF © All rights reserved, P: +45 31 322 322, E-mail: info@aquestionof.dk</div>
+				<div class="meta">A QUESTION OF © All rights reserved, P: +45 31 322 322, E-mail: info@aquestionof.net</div>
 												
 			</div><!-- #footer -->
 		
